70524a17a7aaa23a3aaae517983c15056f59db81
Tim Murphy <tim.murphy86@gmail.com>
2017-05-31 15:14:21 -0400

    code cleanup 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 
 //waiting for user to click the begin button to start game
 // var waitingForAny = false;
-
+// $(document).ready(function(){})
 var globeWord;
 var arrayGuess = [ ];      // Stored array all guessed letters
 var globeAnswer = [ ];    //answers right so far
@@ -21,84 +21,86 @@ pix[4] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/H1.gif"
 pix[5] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/G3.gif"
 pix[6] = "http://img.photobucket.com/albums/v60/PandoraSD/Hangman/G1.gif"
 
+//submit via enter, if game hasn't started yet it will kickoff myJS which will myStart
+document.onkeydown=function(){
+    if(window.event.keyCode=='13'){
+        myJsFunction();
+        }
+    }
 
 //waiting for input
 // document.getElementById("restart").onclick = myStart();
 
 function myStart(){
-console.log("Start Page")
+    console.log("Start Page")
 
-// Setting variable
-var life = 7;
-guessLeft = life;        // Lives
+    // Setting variable
+    // var life = 7;
+    guessLeft = 7;        // Lives
+    arrayGuess=[];
 
-//word for hanging the space man
-var words = ["monitor", "program", "application", "keyboard", "javascript", "gaming", "network"];
 
-// random word from words array
-var word = words[Math.floor(Math.random()*words.length)];
+    //cleaning old html from a refresh
+    document.getElementById("missed").innerHTML = arrayGuess;
 
-//storing selected word into global var
-globeWord=word;
+    //word for hanging the space man
+    var words = ["monitor", "program", "application", "keyboard", "javascript", "gaming", "network"];
 
-//selecting a word and then breaking it into an array
-var letterWords = Array.from(word);
+    // random word from words array
+    var word = words[Math.floor(Math.random()*words.length)];
 
-console.log(letters);
-console.log(words);
-console.log(word);
-console.log(letterWords);
+    //storing selected word into global var
+    globeWord=word;
 
-//Setting up the blank array for the length of the word
-var answerWord = [];
-for (var i = 0; i < word.length; i++) {
-  answerWord[i]="_";
+    //selecting a word and then breaking it into an array
+    var letterWords = Array.from(word);
+
+    console.log(letters);
+    console.log(words);
+    console.log(word);
+    console.log(letterWords);
+
+    //Setting up the blank array for the length of the word
+    var answerWord = [];
+    for (var i = 0; i < word.length; i++) {
+            answerWord[i]="_";
+        }
+    console.log(answerWord);
+    //global var
+    globeAnswer = answerWord;
+
+    document.getElementById("wordDash").innerHTML = globeAnswer;
+    document.getElementById("hangPix").src = pix[6];
+
 }
-console.log(answerWord);
-//global var
-globeAnswer = answerWord;
-
-document.getElementById("wordDash").innerHTML = globeAnswer;
-document.getElementById("hangPix").src = pix[6];
-
-
-//sumbbitting letter guess via clicking Guess or hitting enter
-// document.getElementById("submit")
-//     .addEventListener("keyup", function(event) {
-//     event.preventDefault();
-//     if (event.keyCode == 13) {
-//         document.getElementById(true).click();
-//        }
-//     });
-}
-
-   /* reset letter to guess on click */
-    // guessInput.onclick = function () {
-    //     this.value = '';
-    // };
 
 
 //Input value from input field of html
 function myJsFunction(){
-  console.log("input function runnning");
-  // var letters = "abcdefghijklmnopqrstuvwxyz";
+    console.log("input function runnning");
+
     var text = document.getElementById('input1').value;
+
+    //Clear value in input
+    document.getElementById('input1').value = "";
+
       // Converts the user's answer to lowercase
     var textLower = text.toLowerCase();
     console.log(textLower);
     console.log(letters.includes(textLower));
-    if(((textLower.length) !== 1) && ((letters.includes(textLower))===false)){
-        alert('Invalid Input');
-    }
-    else{
+
+    //checks value to ensure it is a valid input
+    if(textLower.length === 1 && letters.includes(textLower)===true){
         console.log('OK letter input');
         checkGuess(textLower);
-        }
+
+     } else {
+             alert('Invalid Input');
+            }
     }
 
 function checkGuess(letterPass){
   if (typeof globeWord === 'undefined') {
-    // alert("PLEASE CLICK THE START BUTTON TO BEGIN!");
         myStart();
      }
     console.log(letterPass + globeWord);
@@ -109,26 +111,30 @@ function checkGuess(letterPass){
         console.log(i);
         correctLetter(letterPass, i);
         correct--;
-      }
-      else if(correct === (globeWord.length-1)){
-        console.log(globeWord.length);
-        console.log(i);
-        console.log(correct);
+
+      } else if (correct === (globeWord.length-1)){
         wrongLetter(letterPass);
-      }
-      else{
+
+      } else {
         correct++;
         console.log(correct);
+        }
       }
-    }
 }
 
+
 function correctLetter(correct, location){
-  console.log("A correct letter was guess and has kickoff function correctLetter");
   globeAnswer[location] = correct;
-  console.log(globeAnswer);
   document.getElementById("wordDash").innerHTML = globeAnswer;
-}
+
+  //checks for winning status
+  var winCheck = $.inArray(globeAnswer,"_", true);
+  console.log(winCheck);
+  if (winCheck === -1){
+    document.getElementById("left").innerHTML = "Winner!!!";
+  }
+}  
+
 
 function wrongLetter(wrong){
   console.log("A wrong letter was guessed and has kicked off function wrongLetter")
@@ -136,26 +142,28 @@ function wrongLetter(wrong){
   console.log(arrayGuess);
   document.getElementById("missed").innerHTML = arrayGuess;
   lives();
-
- //kickoff hanging drawling
 }
+
+
 function lives(){
   guessLeft--;
   console.log(guessLeft);
+
   if (guessLeft=== 1) {
   var leftStr = "Total Guesses Left: ZERO! You have failed your mission";
   document.getElementById("hangPix").src = pix[(guessLeft-1)];
   console.log(leftStr);
   document.getElementById("left").innerHTML = leftStr;
-
-  }
-  else{
+  
+  } else {
   var leftStr = "Total Guesses Left: " + (guessLeft-1);
   console.log(leftStr);
   document.getElementById("left").innerHTML = leftStr;
   document.getElementById("hangPix").src = pix[(guessLeft-1)];
   }
 }
+
+
 function score(){
   scoreTotals++;
   var scoreStr = "Total Number of Wins: " + scoreTotals;
